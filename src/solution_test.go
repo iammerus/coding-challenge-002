86a6044f6069c5cfdb502de87ec718eeb44c2502
Melvin Mupondori <melmups@outlook.com>
2020-04-10 09:38:32 +0200

    Added unit test

--- src/solution_test.go
+++ src/solution_test.go
@@ -5,5 +5,27 @@ import (
 )
 
 func TestSolution(t *testing.T) {
-	t.Error("Test hasn't been implemented")
+	tests := map[uint64]bool{
+		1023456789:         true,
+		1234567890:         true,
+		8549176320:         true,
+		1234567889:         false,
+		120:                false,
+		3816547290:         true,
+		9814072356:         true,
+		8328393134:         false,
+		9876543210:         true,
+		1224425565:         false,
+		98140723568910:     true,
+		9086452314890:      false,
+		112233445566778899: false,
+	}
+
+	for key, element := range tests {
+		output := isPandigital(key)
+
+		if element != output {
+			t.Errorf("Panidigital test for '%d' was incorrect, expected '%t', got '%t'", key, element, output)
+		}
+	}
 }
